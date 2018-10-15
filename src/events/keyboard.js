2187eb69a420a3558c8b040fc7fc0b601427bbdf
Deven Caron <devencaron@gmail.com>
2018-10-15 09:54:25 -0400

    test fix keyboard events

--- src/events/keyboard.js
+++ src/events/keyboard.js
@@ -17,6 +17,34 @@ const KEY_CODE = {
     DOWN: 40,
 };
 
+const getKeyDelta = (keyCode) => {
+    // key maps [deltaX, deltaY, useSetMethod]
+    const { size, offset, limit, movement } = this; // need real time data
+
+    switch (keyCode) {
+    case KEY_CODE.SPACE:
+        return [0, 200];
+    case KEY_CODE.PAGE_UP:
+        return [0, -size.container.height + 40];
+    case KEY_CODE.PAGE_DOWN:
+        return [0, size.container.height - 40];
+    case KEY_CODE.END:
+        return [0, Math.abs(movement.y) + limit.y - offset.y];
+    case KEY_CODE.HOME:
+        return [0, -Math.abs(movement.y) - offset.y];
+    case KEY_CODE.LEFT:
+        return [-40, 0];
+    case KEY_CODE.UP:
+        return [0, -40];
+    case KEY_CODE.RIGHT:
+        return [40, 0];
+    case KEY_CODE.DOWN:
+        return [0, 40];
+    default:
+        return null;
+    }
+};
+
 /**
  * @method
  * @internal
@@ -24,34 +52,6 @@ const KEY_CODE = {
  */
 function __keyboardHandler() {
     const { targets } = this;
-
-    const getKeyDelta = (keyCode) => {
-        // key maps [deltaX, deltaY, useSetMethod]
-        const { size, offset, limit, movement } = this; // need real time data
-
-        switch (keyCode) {
-        case KEY_CODE.SPACE:
-            return [0, 200];
-        case KEY_CODE.PAGE_UP:
-            return [0, -size.container.height + 40];
-        case KEY_CODE.PAGE_DOWN:
-            return [0, size.container.height - 40];
-        case KEY_CODE.END:
-            return [0, Math.abs(movement.y) + limit.y - offset.y];
-        case KEY_CODE.HOME:
-            return [0, -Math.abs(movement.y) - offset.y];
-        case KEY_CODE.LEFT:
-            return [-40, 0];
-        case KEY_CODE.UP:
-            return [0, -40];
-        case KEY_CODE.RIGHT:
-            return [40, 0];
-        case KEY_CODE.DOWN:
-            return [0, 40];
-        default:
-            return null;
-        }
-    };
 
     const { container } = targets;
 
